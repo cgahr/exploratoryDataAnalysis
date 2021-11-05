bcbd76eb06c9309bd812351d928dbc6b203c3fe5
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2021-11-05 12:29:26 +0100

    new pre-commit config, update box cox notebook

--- scripts/1-3-3-05_box_cox_linearity_plot.py
+++ scripts/1-3-3-05_box_cox_linearity_plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-from src.plots import boxcox_nonlinearity_plot
+from exploratory_data_analysis.plots import boxcox_nonlinearity_plot
 
 # %%
 sns.set_context("notebook")
@@ -11,6 +11,8 @@ sns.set_style("darkgrid")
 
 plt.rcParams["figure.figsize"] = (6, 4)
 plt.rcParams["figure.dpi"] = 120
+
+np.random.seed(12)
 
 
 # %% [markdown]
