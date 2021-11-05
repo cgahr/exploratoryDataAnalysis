c718ababe62d4a7be8b60440736fe9d2d212888d
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2021-11-05 11:12:11 +0100

    updated config files

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="exploratory_data_analysis",
+    version="0.1.0",
+    description="Exploratory Data Analysis Tool Suite",
+    license="TODO",
+    author="Constantin Gahr",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.9",
+    install_requires=["numpy", "matplotlib"],
+    setup_requires=["pytest-runner"],
+    test_suite="pytest",
+    tests_require=["pytest"],
+)
